64cbbc419f6cc5989a6e0c96a494389667b1d3e0
Larissa-Nahan <larissanahan@gmail.com>
2022-07-21 10:06:35 -0300

    Sign in: validando email existente e decriptografando + token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const authRouter = express.Router();
@@ -39,5 +40,32 @@ authRouter.post("/api/signup", async (req, res) => {
   }
 
 });
+
+//Route Sign-In
+authRouter.post("/api/signin", async (req, res) => {
+    try {
+      //Pegar os dados do cliente (no form)
+      const { email, password } = req.body;
+  
+      //(validação que o usuário já existe)
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ msg: "User with this e-mail does not exist" });
+      }
+  
+      //(decriptografando a senha)
+      const isMatch = await bcryptjs.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Incorrect password" });
+      }
+  
+      const token = jwt.sign({ id: user._id }, "passwordKey");
+      res.json({ token, ...user._doc });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
 
 module.exports = authRouter;
