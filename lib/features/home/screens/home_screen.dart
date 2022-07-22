627ae9e5fa92c4dc455e7463ef2d63e5db9f80b1
Larissa-Nahan <larissanahan@gmail.com>
2022-07-22 09:51:53 -0300

    Design home page complete (temporary deal)

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:amazon/constants/global_variables.dart';
 import 'package:amazon/features/home/widgets/address_box.dart';
 import 'package:amazon/features/home/widgets/carousel_image.dart';
+import 'package:amazon/features/home/widgets/deal_day.dart';
 import 'package:amazon/features/home/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
 
@@ -89,14 +90,17 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: const [
-          AddressBox(),
-          SizedBox(height: 10),
-          TopCategories(),
-          SizedBox(height: 10),
-          CarouselImage(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            AddressBox(),
+            SizedBox(height: 10),
+            TopCategories(),
+            SizedBox(height: 10),
+            CarouselImage(),
+            DealOfDay(),
+          ],
+        ),
       ),
     );
   }
