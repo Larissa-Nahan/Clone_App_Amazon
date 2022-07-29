2ee17220409258a11c0e1361262ee36d5044a088
Larissa-Nahan <larissanahan@gmail.com>
2022-07-29 11:11:56 -0300

    Changing uri

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = "http://amazon-clone-testing.heroku.com:3001";
+String uri = "http://amazon-clone-testing.heroku.com";
 
 class GlobalVariables {
   // Cores
